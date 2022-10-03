fa8015a0bd914c4d0d2d49fdf557ab83b9f23966
sagentoft <soren.agentoft@gmail.com>
2022-10-03 10:12:51 +0200

    Created repo

---
+++ PrototypingAssignment/Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public enum CameraState {
+        Gameplay,
+        ScriptedCinematic,
+        Death
+    }
+
+    [Header("References and Misc")]
+    public GameObject player;
+    public CameraState currentState = new CameraState();
+
+    [Header("Gameplay State following Parameters")]
+    public bool shouldAlwaysFollowPlayerInGameplay = false;
+
+    public Vector2 screenEdgeForFollow;
+
+    public float followDamping;
+
+    public AnimationCurve dampingScreenEdgeDistance;
+
+    private Camera cam;
+
+    private Vector3 initialCameraOffset;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        initialCameraOffset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        switch (currentState)
+        {
+            case CameraState.Gameplay:
+                UpdateOnGameplay();
+                break;
+
+            case CameraState.Death:
+                UpdateOnDeath();
+                break;
+
+            case CameraState.ScriptedCinematic:
+                UpdateOnScriptedCinematic();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private void UpdateOnGameplay()
+    {
+        if (shouldAlwaysFollowPlayerInGameplay)
+        {
+            transform.position = Vector3.Lerp(transform.position, player.transform.position + initialCameraOffset, followDamping * Time.deltaTime);
+            return;
+        }
+
+        Vector3 playerRelativeScreenPosition = cam.WorldToViewportPoint(player.transform.position);
+        float adjustedDamping = followDamping;
+
+        if(playerRelativeScreenPosition.x <= screenEdgeForFollow.x)
+        {
+            adjustedDamping *= dampingScreenEdgeDistance.Evaluate(1-(playerRelativeScreenPosition.x / screenEdgeForFollow.x));
+            transform.Translate(transform.right * Time.deltaTime * adjustedDamping * -1);
+        }
+        if(playerRelativeScreenPosition.x > (1 - screenEdgeForFollow.x))
+        {
+            adjustedDamping *= dampingScreenEdgeDistance.Evaluate(1-((1-playerRelativeScreenPosition.x) / screenEdgeForFollow.x));
+            transform.Translate(transform.right * Time.deltaTime * adjustedDamping);
+        }
+        if(playerRelativeScreenPosition.y <= screenEdgeForFollow.y)
+        {
+            adjustedDamping *= dampingScreenEdgeDistance.Evaluate(1-(playerRelativeScreenPosition.y / screenEdgeForFollow.y));
+            transform.position -= Vector3.forward * Time.deltaTime * adjustedDamping;
+        }
+        if(playerRelativeScreenPosition.y > (1 - screenEdgeForFollow.y))
+        {
+            adjustedDamping *= dampingScreenEdgeDistance.Evaluate(1 - ((1-playerRelativeScreenPosition.y) / screenEdgeForFollow.y));
+            transform.position += Vector3.forward * Time.deltaTime * adjustedDamping;
+        }
+    }
+
+    private void UpdateOnScriptedCinematic()
+    {
+
+    }
+
+    private void UpdateOnDeath()
+    {
+
+    }
+}
---
+++ PrototypingAssignment/Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    [Header("Base Speed Values")]
+    public float maximumSpeed;
+    public float gravityValue;
+
+    [Header("Acceleration Curves")]
+    public AnimationCurve accelerationCurve;
+    public AnimationCurve decelerationCurve;
+    public float accelerationDuration;
+    public float decelerationDuration;
+
+    private float targetSpeed;
+    private float speedChangeTimer = 0;
+    private float lastVelocitybeforeDeceleration = 0;
+
+    private CharacterController controller;
+    private Transform playerBody;
+
+    private Vector3 movementVector;
+    private Vector3 lastMaxMovementVector;
+
+    private Vector2 leftStickPosition;
+    private Vector2 rightStickPosition;
+
+    [HideInInspector]
+    public bool isInputEnabled;
+    private bool isDecelerating = false;
+
+
+    //@INIT
+    void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+        playerBody = transform.GetChild(0);
+        movementVector = Vector3.zero;
+        targetSpeed = maximumSpeed;
+    }
+
+    void Update()
+    {
+        float dt = Time.deltaTime; //It's used quite a lot in this function so I am storing it locally for a light optimization
+
+        controller.Move(new Vector3(0, -gravityValue * dt, 0)); //gravity: the character constantly moves downwards, the CharacterController component handles collision
+        
+        float progression = 0;
+
+        //What happens when the stick is pressed in a direction
+        if(leftStickPosition != Vector2.zero)
+        {
+            isDecelerating = false;
+
+            //Acceleration
+            if(speedChangeTimer <= accelerationDuration)
+            {
+                speedChangeTimer += dt;
+                progression = Mathf.Clamp(speedChangeTimer / accelerationDuration, 0, 1);
+                targetSpeed = maximumSpeed * accelerationCurve.Evaluate(progression);
+
+                movementVector = Vector3.Lerp(Vector3.zero, new Vector3(leftStickPosition.x, 0, leftStickPosition.y) * targetSpeed, progression);
+                lastMaxMovementVector = movementVector.normalized * maximumSpeed;
+            }
+            //Top speed
+            else
+            {
+                movementVector = new Vector3(leftStickPosition.x, 0, leftStickPosition.y) * maximumSpeed;
+                lastMaxMovementVector = movementVector;
+            }
+        }
+        else
+        {
+            //Deceleration
+            if(speedChangeTimer > 0)
+            {
+                //This happens only once per deceleration at the first frame
+                if (!isDecelerating)
+                {
+                    isDecelerating = true;
+                    lastVelocitybeforeDeceleration = movementVector.magnitude;
+                }
+
+                if (speedChangeTimer > decelerationDuration)
+                    speedChangeTimer = decelerationDuration; //Clamping the time value to the deceleration
+
+                float ratio = lastVelocitybeforeDeceleration < maximumSpeed ? lastVelocitybeforeDeceleration / maximumSpeed : 1; //Ternary operator, useful to avoid small if/else statements
+
+                speedChangeTimer -= dt;
+                progression = Mathf.Clamp(speedChangeTimer / decelerationDuration, 0, 1);
+                movementVector = Vector3.Lerp(Vector3.zero, lastMaxMovementVector * ratio, progression);
+            }
+        }
+
+        //All the previous code only serves to calculate the movementVector, and we apply the movement to the controller at the very end of the Update
+        controller.Move(movementVector * dt);
+    }
+
+    //@INPUT: Gather the stick coordinates
+    private void OnMove(InputValue movementValue)
+    {
+        leftStickPosition = movementValue.Get<Vector2>();
+    }
+
+    //@INPUT: Handle transform rotation
+    private void OnLook(InputValue lookValue)
+    {
+        rightStickPosition = lookValue.Get<Vector2>();
+
+        if(rightStickPosition != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(rightStickPosition.x, rightStickPosition.y) * Mathf.Rad2Deg;
+            playerBody.rotation = Quaternion.Euler(0, angle - 90, 0);
+        }
+    }
+}
